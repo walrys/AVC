7996a4d462da100402a17e75966a4bb297182ce2
Walrus <walrus@hotmail.sg>
2016-10-19 15:44:53 +0800

    fixed bugs in audioprocessor

--- classifier/classify_wh.py
+++ classifier/classify_wh.py
@@ -1,30 +1,11 @@
 import classify
 import numpy as np
 
-
-def arrtodict(array):
-    dict = {}
-    for item in array:
-        dict[str(item[0])] = item[1].ravel()
-        
-    return dict
-
-def dicttoarray(dict):
-    array  = []
-    for item in dict:
-        x = []
-        for line in dict[item]:
-            print len(line)
-            #x.append(line[:37])
-        x = x.ravel()
-        array.append(x)
-        
-    return array
     
 if __name__ == '__main__':
-    type = 'energy'
-    train_energy = np.load('train_'+type+'.npy').item()
-    valid_energy = np.load('valid_'+type+'.npy').item()
+    type = 'emsz_short'
+    train_energy = np.load('train_'+type+'.npy')
+    valid_energy = np.load('valid_'+type+'.npy')
     train_gnd = np.load('train_gnd.npy')
     train_gnd = train_gnd[:,1]
     valid_gnd = np.load('valid_gnd.npy')
@@ -32,19 +13,11 @@ if __name__ == '__main__':
 
     venues = np.load('venues.npy')
     venues = venues[:,1]
-
-    #give it an array
-    '''
-    dataset_train = [[1,2,3,4,5,1],[1,2,3,4,5,1],[1,2,3,4,5,1],[1,2,3,4,5,1],[1,2,3,4,5,1]]
-    dataset_validation = [[1,2,3,4,5,1],[1,2,3,4,5,1],[1,2,3,4,5,1],[1,2,3,4,5,1],[1,2,3,4,5,1]]
-    dataset_train_label   = [1,2,3,4,5]
-    dataset_validation_label   = [1,2,3,4,5]
-    dataset_label_names = ['one','two','three','four','five']
-    '''
+    
     #STUFF WE NEED:
-    dataset_train = dicttoarray(train_energy)
+    dataset_train = train_energy
     dataset_train_label = train_gnd
-    dataset_validation = dicttoarray(valid_energy)
+    dataset_validation = valid_energy
     dataset_validation_label = valid_gnd
     dataset_label_names = venues
 
